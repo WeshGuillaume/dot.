cabc068b6832a6c771173b556ba56bd91db75998
Guillaume <guillaume.badi@gmail.com>
2017-07-16 17:04:16 +0200

    simple JSON parser

--- src/example.js
+++ src/example.js
@@ -1,11 +1,13 @@
 
 const { noneOf, digit, char, letter } = require('./chars')
-const { between, oneOf, many1, sequence, endBy, skip, sepBy } = require('./combinators')
+const { many, maybe, between, oneOf, many1, sequence, endBy, skip, sepBy } = require('./combinators')
 const { symbol } = require('./strings')
 const { createState } = require('./state')
 
 const string = state =>
   sequence(skip(char('"')), endBy(char('"'))(noneOf('"')))(state)[0].join('')
+
+const space = oneOf(char(' '), char('\t'), char('\n'))
 
 const openBrace = char('{')
 const closeBrace = char('}')
@@ -13,16 +15,21 @@ const openBracket = char('[')
 const closeBracket = char(']')
 const comma = char(',')
 
-const key = string
-
 const number = state => parseInt(many1(digit)(state).join(''))
+
+const lexeme = p => state => {
+  const ret = sequence(skip(maybe(many(space))), p, skip(maybe(many(space))))(state)
+  return ret[0]
+}
+
+const key = lexeme(string)
 
 const T = symbol('true')
 const F = symbol('false')
 const boolean = oneOf(T, F)
 
 function value (state) {
-  return oneOf(number, string, array, object)(state)
+  return lexeme(oneOf(boolean, string, array, object, number))(state)
 }
 
 function array (state) {
@@ -30,17 +37,33 @@ function array (state) {
 }
 
 const pair = state => {
-  return sequence(key, skip(char(':')), value)(state)
+  const ret = lexeme(sequence(key, skip(char(':')), value))(state)
+  return { [ret[0]]: ret[1] }
 }
 
 function propsList (state) {
-  return sepBy(comma)(pair)(state)
+  const ret = sepBy(comma)(pair)(state)
+  return Object.assign(...ret)
 }
 
 function object (state) {
   return between(char('{'), char('}'))(propsList)(state)
 }
 
-const state = createState('{"name":["hello",{"hello":"world"}],"age":23}')
-const parser = object
-console.log(parser(state), state())
+const state = createState(`
+{
+  "name": "guillaume",
+  "age": 23,
+  "city": {
+    "name": "Paris",
+    "streets": [
+      { "name": "crimée" },
+      { "name": "jean-jaures" }
+    ]
+  }
+}
+`)
+
+const parser = value
+const ret = parser(state)
+console.log(ret, state())
