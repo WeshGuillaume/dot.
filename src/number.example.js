cc9a7c585458b6cabdb13d908265a8d30c696381
Guillaume Badi <GuillaumeBadi@users.noreply.github.com>
2017-07-20 01:21:34 +0200

    Delete number.example.js

--- src/number.example.js
+++
@@ -1,9 +0,0 @@
-
-import numbers from './numbers'
-import { createState } from './state'
-
-const number = 1231.214
-
-const state = createState(`${number}`)
-const parser = numbers.float
-console.log(parser(state), state())
