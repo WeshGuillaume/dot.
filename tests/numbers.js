555f5a2b1d48abadfdd3b1c796c0516f50d2adcf
Guillaume Badi <guillaume.badi@gmail.com>
2017-07-21 17:13:55 +0200

    unit-tests + try date

--- tests/numbers.js
+++ tests/numbers.js
@@ -1,8 +1,22 @@
 
+import { expect } from 'chai'
+
+import { createState } from '../src/state'
+import { number, integer, float } from '../src/numbers'
+
+function testSuccess (parser, input, output) {
+  it(`Success: ${parser.parserName}`, () => {
+    const state = createState({ input })
+    const result = parser(state)
+    expect(result.value.return).to.equal(output)
+    expect(result.value.error).to.equal(null)
+  })
+}
+
 describe('Numbers', () => {
 
-  it('match an integer')
-  it('match a float')
-  it('match a number')
+  testSuccess(integer, '123d', '123')
+  testSuccess(float, '1.2s', '1.2')
+  testSuccess(number, '213.23', '213.23')
 
 })
